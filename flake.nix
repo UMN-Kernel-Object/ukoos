415b021e700788717c26dd99b81b4398f351fe43
Nathan Ringo <me@remexre.com>
2025-06-17 14:36:08 -0500

    Adds a Nix flake.

---
+++ flake.nix
@@ -0,0 +1,54 @@
+{
+  outputs =
+    {
+      self,
+      flake-utils,
+      nixpkgs,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      rec {
+        devShells.default = pkgs.mkShell {
+          inputsFrom = builtins.attrValues packages;
+          nativeBuildInputs = [
+            pkgs.bear
+            pkgs.qemu
+          ];
+        };
+
+        packages.default = pkgs.stdenvNoCC.mkDerivation {
+          pname = "ukoos";
+          version = "git";
+
+          src = ./.;
+          nativeBuildInputs = [
+            pkgs.getopt
+            pkgs.pkgsCross.riscv64-embedded.stdenv.cc.bintools.bintools
+            pkgs.pkgsCross.riscv64-embedded.stdenv.cc.cc
+            pkgs.python3
+          ];
+
+          dontUnpack = true;
+          configurePhase = ''
+            runHook preConfigure
+
+            mkdir build
+            cd build
+            bash $src/configure
+
+            runHook postConfigure
+          '';
+          installPhase = ''
+            runHook preInstall
+
+            make install DESTDIR=$out
+
+            runHook postInstall
+          '';
+        };
+      }
+    );
+}
