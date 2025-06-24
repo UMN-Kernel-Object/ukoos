127f6778145d4d115c6c3dae92873be792f6ea85
Nathan Ringo <me@remexre.com>
2025-06-23 21:00:55 -0500

    bootstub: Pass the range the kernel is in.
    This will let us avoid treating those pages as allocatable memory, and
    will let us ensure we _can_ treat the ones we want to as allocatable
    later.
    
    In particular, the initial page tables and initial stack should both be
    reclaimable -- we make sure they're covered by the buddy allocator we
    set up, so whenever they get freed, they can be added back to the buddy
    system. (We'll do the same for the DeviceTree as well.)

--- src/kernel/main.c
+++ src/kernel/main.c
@@ -4,8 +4,9 @@
 #include <symbolicate.h>
 
 [[noreturn]]
-void main(u64 hart_id, paddr devicetree_start, const struct Elf64_Sym *symtab,
-          usize symtab_len, const char *strtab, usize strtab_len) {
+void main(u64 hart_id, paddr devicetree_start, paddr kernel_start,
+          paddr kernel_end, const struct Elf64_Sym *symtab, usize symtab_len,
+          const char *strtab, usize strtab_len) {
   print("Starting to boot ukoOS...");
   symbolicate_init(symtab, symtab_len, strtab, strtab_len);
   devicetree_init(devicetree_start);
