b45b751b5ebb9f317080fc8e7f6019aab1bcc690
Nathan Ringo <me@remexre.com>
2025-06-23 22:03:43 -0500

    Log what mm_init_physical will do.

--- src/kernel/mm/physical_alloc.c
+++ src/kernel/mm/physical_alloc.c
@@ -1,6 +1,7 @@
 #include <mm/physical_alloc.h>
 #include <panic.h>
 #include <physical.h>
+#include <print.h>
 
 // A singly linked list of allocatable chunks of physical memory. The first word
 // of every chunk is the next pointer, the second word is the number of pages in
@@ -11,14 +12,42 @@ void mm_init_add_physical_chunk(paddr start, paddr end) {
   assert(!start.offset);
   assert(!end.offset);
 
-  physical_write_u64le(paddr_offset(start, 0),
-                       paddr_to_bits(init_physical_allocator));
+  physical_write_u64le(start, paddr_to_bits(init_physical_allocator));
   physical_write_u64le(paddr_offset(start, sizeof(paddr)),
                        (paddr_to_bits(end) - paddr_to_bits(start)) >> 12);
   init_physical_allocator = start;
 }
 
+static void union_range(paddr *start1, paddr *end1, paddr start2, paddr end2) {
+  if (paddr_to_bits(start2) < paddr_to_bits(*start1))
+    *start1 = start2;
+  if (paddr_to_bits(end2) > paddr_to_bits(*end1))
+    *end1 = end2;
+}
+
 void mm_init_physical(paddr devicetree_start, paddr devicetree_end,
                       paddr kernel_start, paddr kernel_end) {
+  // Compute the range we need to handle allocating physical memory from, and
+  // log the chunks of memory we've been given.
+  devicetree_start = paddr_align_up(devicetree_start, 12);
+  devicetree_end = paddr_align_down(devicetree_end, 12);
+  kernel_start = paddr_align_up(kernel_start, 12);
+  kernel_end = paddr_align_down(kernel_end, 12);
+  paddr all_physical_start = devicetree_start,
+        all_physical_end = devicetree_end;
+  union_range(&all_physical_start, &all_physical_end, devicetree_start,
+              devicetree_end);
+  print("Allocatable memory chunks:");
+  for (paddr next_chunk = init_physical_allocator; paddr_to_bits(next_chunk);
+       next_chunk = paddr_of_bits(physical_read_u64le(next_chunk))) {
+    paddr chunk_start = next_chunk;
+    u64 length = physical_read_u64le(paddr_offset(next_chunk, sizeof(paddr)));
+    paddr chunk_end = paddr_offset(chunk_start, length << 12);
+    union_range(&all_physical_start, &all_physical_end, chunk_start, chunk_end);
+    print("  {paddr}-{paddr}", chunk_start, chunk_end);
+  }
+  print("Preparing the physical allocator to handle {paddr}-{paddr}...",
+        all_physical_start, all_physical_end);
+
   TODO();
 }
