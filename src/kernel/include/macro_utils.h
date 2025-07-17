21a13e0cfa52d8edd3735405dc70401588caae05
Nathan Ringo <me@remexre.com>
2025-07-17 06:37:24 -0500

    Adds max and min macros.
    These only compare two values of the same type, and avoid expanding to
    exponentially much code when something like min(a, min(b, min(c, d))) is
    encountered.

--- src/kernel/include/macro_utils.h
+++ src/kernel/include/macro_utils.h
@@ -1,6 +1,19 @@
 #ifndef UKO_OS_KERNEL__MACRO_UTILS_H
 #define UKO_OS_KERNEL__MACRO_UTILS_H 1
 
+#define __HACKY_INTEGRAL_TYPEID(EXPR)                                          \
+  _Generic((EXPR),                                                             \
+      i8: 1,                                                                   \
+      u8: 0,                                                                   \
+      i16: 3,                                                                  \
+      u16: 2,                                                                  \
+      i32: 5,                                                                  \
+      u32: 4,                                                                  \
+      i64: 9,                                                                  \
+      u64: 8,                                                                  \
+      isize: 7,                                                                \
+      usize: 6)
+
 #define __PASTE(L, R) L##R
 
 #endif // UKO_OS_KERNEL__MACRO_UTILS_H
---
+++ src/kernel/include/minmax.h
@@ -0,0 +1,48 @@
+#ifndef UKO_OS_KERNEL__MINMAX_H
+#define UKO_OS_KERNEL__MINMAX_H 1
+
+#include <macro_utils.h>
+#include <types.h>
+
+#define __DEFINE_MINMAX_FOR_TYPE(TYPE)                                         \
+  [[unsequenced]]                                                              \
+  static inline TYPE max_##TYPE(TYPE lhs, TYPE rhs) {                          \
+    return (lhs > rhs) ? lhs : rhs;                                            \
+  }                                                                            \
+                                                                               \
+  [[unsequenced]]                                                              \
+  static inline TYPE min_##TYPE(TYPE lhs, TYPE rhs) {                          \
+    return (lhs < rhs) ? lhs : rhs;                                            \
+  }
+
+__DEFINE_MINMAX_FOR_TYPE(i8)
+__DEFINE_MINMAX_FOR_TYPE(u8)
+__DEFINE_MINMAX_FOR_TYPE(i16)
+__DEFINE_MINMAX_FOR_TYPE(u16)
+__DEFINE_MINMAX_FOR_TYPE(i32)
+__DEFINE_MINMAX_FOR_TYPE(u32)
+__DEFINE_MINMAX_FOR_TYPE(i64)
+__DEFINE_MINMAX_FOR_TYPE(u64)
+
+#define max(LHS, RHS)                                                          \
+  ({                                                                           \
+    const auto __max_LHS = (LHS);                                              \
+    const auto __max_RHS = (RHS);                                              \
+    static_assert(__HACKY_INTEGRAL_TYPEID(__max_LHS) ==                        \
+                      __HACKY_INTEGRAL_TYPEID(__max_RHS),                      \
+                  "cannot compute max(" #LHS ", " #RHS                         \
+                  "), when the two arguments have different types");           \
+    (__max_LHS > __max_RHS) ? __max_LHS : __max_RHS;                           \
+  })
+#define min(LHS, RHS)                                                          \
+  ({                                                                           \
+    const auto __min_LHS = (LHS);                                              \
+    const auto __min_RHS = (RHS);                                              \
+    static_assert(__HACKY_INTEGRAL_TYPEID(__min_LHS) ==                        \
+                      __HACKY_INTEGRAL_TYPEID(__min_RHS),                      \
+                  "cannot compute min(" #LHS ", " #RHS                         \
+                  "), when the two arguments have different types");           \
+    (__min_LHS < __min_RHS) ? __min_LHS : __min_RHS;                           \
+  })
+
+#endif // UKO_OS_KERNEL__MINMAX_H
