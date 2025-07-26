2a50ea846ac5f273dcf98e39fd96c7a8e03ee277
Amy Ringo <me@remexre.com>
2025-07-26 12:48:55 -0500

    Adds initializers and documentation about them.
    Signed-off-by: Amy Ringo <me@remexre.com>

--- src/kernel/include/init.h
+++ src/kernel/include/init.h
@@ -10,14 +10,49 @@
 #include <macro_utils.h>
 #include <types.h>
 
+/**
+ * The pre-defined priority levels.
+ */
+enum initializer_priority : i32 {
+  /**
+   * The priority level at which drivers should register themselves.
+   */
+  INIT_REGISTER_DRIVERS = 10,
+};
+
+/**
+ * An initializer function.
+ */
 struct initializer {
+  /**
+   * The actual function to run.
+   */
   void (*func)(void);
+
+  /**
+   * The path to the file the initializer is defined in.
+   */
   const char *file;
+
+  /**
+   * The line the initializer is defined on.
+   */
   u32 line;
-  i32 priority;
+
+  /**
+   * The priority of the initializer.
+   */
+  enum initializer_priority priority;
 };
 
 #define _DEFINE_INIT(PRIORITY, N)                                              \
+  static_assert(__builtin_classify_type(typeof((PRIORITY))) ==                 \
+                    __builtin_classify_type(enum initializer_priority),        \
+                "PRIORITY must be an enum initializer_priority");              \
+  static_assert(                                                               \
+      _Generic((PRIORITY), enum initializer_priority: true, default: false),   \
+      "PRIORITY must be an enum initializer_priority");                        \
+                                                                               \
   static void __PASTE(__init_func_, N)(void);                                  \
   [[gnu::section(".initializers,\"wa\",@progbits#"), gnu::used]]               \
   static struct initializer __PASTE(__initializer_, N)[1] = {{                 \
@@ -29,7 +64,7 @@ struct initializer {
   static void __PASTE(__init_func_, N)(void)
 
 /**
- * Defines an initializer.
+ * Defines an initializer. `PRIORITY` should be an `enum initializer_priority`.
  */
 #define DEFINE_INIT(PRIORITY) _DEFINE_INIT(PRIORITY, __COUNTER__)
 
