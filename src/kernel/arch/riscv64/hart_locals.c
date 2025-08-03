4fe92b89f22f917e18ba75527862cc787e5d5991
Nathan Ringo <me@remexre.com>
2025-08-02 22:41:51 -0500

    Changes hart-locals to be stored in SSCRATCH instead of tp.
    This lets us use hart-locals inside trap handlers.
    
    Signed-off-by: Nathan Ringo <me@remexre.com>

--- src/kernel/arch/riscv64/hart_locals.c
+++ src/kernel/arch/riscv64/hart_locals.c
@@ -1,3 +1,4 @@
+#include <arch/riscv64/insns.h>
 #include <hart_locals.h>
 
 /**
@@ -20,15 +21,11 @@ static __attribute__((aligned(1 << MM_ALLOC_SEGMENT_SHIFT))) union {
 } boothart_heap_segment;
 
 struct hart_locals *get_hart_locals(void) {
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wuninitialized"
-  register struct hart_locals *tp __asm__("tp");
-  return tp;
-#pragma GCC diagnostic pop
+  return (struct hart_locals *)csrr(RISCV64_CSR_SSCRATCH);
 }
 
 void init_boothart_hart_locals(u64 hart_id) {
-  __asm__ volatile("la tp, %0\n" : : "i"(&boothart_hart_locals) :);
+  csrw(RISCV64_CSR_SSCRATCH, (u64)&boothart_hart_locals);
   boothart_hart_locals = (struct hart_locals){
       .hart_id = hart_id,
       .heap = &boothart_heap,
