ff679bb04196eccf945eb2b63bc0c32c4e6e9d85
theduccom <145524869+theduccom@users.noreply.github.com>
2025-08-24 14:51:17 -0500

    Add interrupt disabling to spinlock

--- src/kernel/include/spinlock.h
+++ src/kernel/include/spinlock.h
@@ -1,13 +1,14 @@
 #ifndef UKO_OS_KERNEL__SPINLOCK_H
 #define UKO_OS_KERNEL__SPINLOCK_H 1
 
+#include "hart_locals.h"
 #include "types.h"
 
 struct spinlock {
 	u32 state;
 	const char *name;
-	// struct *cpu something?
-}
+	struct hart_locals *hart;
+};
 
 void initlock(struct spinlock *sp, const char *name);
 
