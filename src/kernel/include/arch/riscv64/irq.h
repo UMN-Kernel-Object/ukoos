a24333425bebaeb7e8aa8e2c559299dc9a2b9118
theduccom <145524869+theduccom@users.noreply.github.com>
2025-08-29 18:57:18 -0500

    Add SIE bit handling
    Signed-off-by: theduccom <145524869+theduccom@users.noreply.github.com>

---
+++ src/kernel/include/arch/riscv64/irq.h
@@ -0,0 +1,40 @@
+#ifndef UKO_OS_KERNEL__IRQ_H
+#define UKO_OS_KERNEL__IRQ_H 1
+
+#include "insns.h"
+#include "types.h"
+
+#define RISCV64_SSTATUS_SIE (1UL << 1)
+
+/**
+ * irq_enable - Enables interrupt requests.
+ *
+ * Sets the SIE (supervisor interrupt enable) flag in the SSTATUS CSR.
+ */
+static inline void irq_enable(void) {
+	u64 sstatus = csrr(RISCV64_CSR_SSTATUS);
+	csrw(RISCV64_CSR_SSTATUS, (sstatus | RISCV64_SSTATUS_SIE));
+}
+
+/**
+ * irq_disable - Disables interrupt requests.
+ *
+ * Masks the SIE (supervisor interrupt enable) flag in the SSTATUS CSR.
+ */
+static inline void irq_disable(void) {
+	u64 sstatus = csrr(RISCV64_CSR_SSTATUS);
+	csrw(RISCV64_CSR_SSTATUS, (sstatus & ~RISCV64_SSTATUS_SIE));
+}
+
+/**
+ * get_irq_status - Gets the state of the SSTATUS SIE bit.
+ *
+ * Return: u32 1 if SIE bit in SSTATUS is set, 0 otherwise.
+ */
+static inline u32 get_irq_state(void) {
+	u64 sstatus = csrr(RISCV64_CSR_SSTATUS);
+	return (sstatus & RISCV64_SSTATUS_SIE) != 0;
+}
+
+#endif  // UKO_OS_KERNEL__IRQ_H
+
