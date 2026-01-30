f3fd82a81795493e9a2f56cc951878819544b52f
Autumn <auctumnus@pm.me>
2026-01-30 09:25:47 -0600

    feat: spinlocks
    Signed-off-by: Autumn <auctumnus@pm.me>

---
+++ src/kernel/include/arch/riscv64/irq.h
@@ -0,0 +1,58 @@
+/*
+ * SPDX-FileCopyrightText: 2026 ukoOS Contributors
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+#ifndef UKO_OS_KERNEL__ARCH_RISCV64_IRQ_H
+#define UKO_OS_KERNEL__ARCH_RISCV64_IRQ_H 1
+
+/**
+ * IRQ handling.
+ */
+
+#include <insns.h>
+#include <types.h>
+
+// TODO: move this over to insns.h?
+#define RISCV64_CSR_SSTATUS_SIE (1 << 1)
+
+// TODO: consider making these common across architectures somehow?
+// linux has `arch_local_irq­_*` and then calls those from `local_irq_*`
+// for now, riscv64 is the only target anyways, so ...
+
+/**
+ * Disables local interrupts.
+ */
+static inline void local_irq_disable(void) {
+  csrc(RISCV64_CSR_SSTATUS, RISCV64_CSR_SSTATUS_SIE); // clear SIE bit
+}
+
+/**
+ * Enables local interrupts.
+ */
+static inline void local_irq_enable(void) {
+  csrs(RISCV64_CSR_SSTATUS, RISCV64_CSR_SSTATUS_SIE); // set SIE bit
+}
+
+/**
+ * Saves the current interrupt state and disables local interrupts.
+ *
+ * @return The previous interrupt state flags.
+ */
+static inline u64 local_irq_save(void) {
+  u64 sstatus =
+      csrrc(RISCV64_CSR_SSTATUS, RISCV64_CSR_SSTATUS_SIE); // clear SIE bit
+  return sstatus & RISCV64_CSR_SSTATUS_SIE;
+}
+
+/**
+ * Restores the local interrupt state from the given flags.
+ */
+static inline void local_irq_restore(u64 flags) {
+  if (flags & RISCV64_CSR_SSTATUS_SIE) {
+    local_irq_enable();
+  }
+}
+
+#endif // UKO_OS_KERNEL__ARCH_RISCV64_IRQ_H
---
+++ src/kernel/spinlock.c
@@ -0,0 +1,87 @@
+#include <hart_locals.h>
+#include <panic.h>
+#include <spinlock.h>
+#include <types.h>
+// TODO: make generic across architectures
+#include <arch/riscv64/insns.h>
+#include <arch/riscv64/irq.h>
+
+// None of these functions should be called manually.
+// See doc/kernel/containers/guards.md for usage.
+
+void __spinlock_guard_init(struct spinlock_guard *guard,
+                           struct spinlock *spinlock, const char *file,
+                           usize line, const char *func) {
+  // Not really any reason this should happen in practice.
+  if (spinlock == nullptr) {
+    panic("in {cstr} from {cstr}:{usize}: passed a null spinlock", func, file,
+          line);
+  }
+
+  // When acquiring a spinlock, disable local interrupts.
+  // Otherwise, we might get interrupted and then attempt to re-acquire the same
+  // spinlock, leading to a deadlock.
+  u64 flags = local_irq_save();
+
+  struct hart_locals *locals = get_hart_locals();
+  u64 hart_id = locals->hart_id;
+
+  if (spinlock->owner_hart_id == hart_id) {
+    panic("hart {u64} tried to re-acquire spinlock it already owns "
+          "from {cstr}:{usize} in {cstr}",
+          hart_id, file, line, func);
+  }
+
+  bool expected = false;
+
+  // Try to acquire the lock.
+  while (!__atomic_compare_exchange_n(&spinlock->locked, &expected, true, false,
+                                      __ATOMIC_ACQUIRE, __ATOMIC_RELAXED)) {
+    expected = false;
+    pause_hint();
+  }
+
+  spinlock->flags = flags;
+
+  spinlock->owner_hart_id = hart_id;
+  spinlock->file = file;
+  spinlock->line = line;
+  spinlock->func = func;
+
+  guard->spinlock = spinlock;
+}
+
+void __spinlock_guard_free(struct spinlock_guard *guard) {
+  if (guard == nullptr) {
+    panic("spinlock guard free called with null guard");
+  }
+
+  struct spinlock *spinlock = guard->spinlock;
+  if (spinlock == nullptr) {
+    // TODO: warn here?
+    return;
+  }
+
+  struct hart_locals *locals = get_hart_locals();
+  u64 hart_id = locals->hart_id;
+
+  if (spinlock->owner_hart_id != hart_id) {
+    panic("hart {u64} tried to release spinlock owned by hart {u64} "
+          "from {cstr}:{usize} in {cstr}",
+          hart_id, spinlock->owner_hart_id, spinlock->file, spinlock->line,
+          spinlock->func);
+  }
+
+  spinlock->owner_hart_id = 0;
+  spinlock->file = nullptr;
+  spinlock->line = 0;
+  spinlock->func = nullptr;
+
+  // Release the lock.
+  u64 flags = spinlock->flags;
+  guard->spinlock = nullptr;
+  __atomic_store_n(&spinlock->locked, false, __ATOMIC_RELEASE);
+
+  // Re-enable local interrupts.
+  local_irq_restore(flags);
+}
