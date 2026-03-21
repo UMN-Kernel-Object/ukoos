0a979098aceee1af0a31b5a836a3264fdd92e55e
jastintime <130672671+jastintime@users.noreply.github.com>
2026-03-21 11:21:13 -0500

    ipv6.h: fix size of fields, add static_assert, unusued structs
    we don't have 128 bit integers in the kernel instead we can treat the
    addresses as  128 bit wide arrays.
    
    static_assert to make sure the header is the required 40 octets.
    
    comment out unusued structs, we don't have to worry about routing or
    options as we are not routers or interacting with neighbors.
    
    Signed-off-by: jastintime <130672671+jastintime@users.noreply.github.com>

--- src/kernel/include/net/ipv6.h
+++ src/kernel/include/net/ipv6.h
@@ -10,13 +10,11 @@
 enum IPv6ProtoNum : u8 { HOPOPT = 0, ICMP = 1, TCP = 6, UDP = 17 };
 
 struct ipv6_header {
-  u32 version : 4;
-  u32 traffic_class : 8;
-  u32 flow_label : 20;
-
+  u8 vtf[4];
   u16 payload_len;
   u8 next_header;
   u8 hop_limit;
+  // TODO: put these in a struct ip_address
   u8 src[16];
   u8 dst[16];
 };
@@ -54,6 +52,6 @@ struct fragment_header {
 //}
 //
 
-long ipv6_send_packet(struct ipv6_header header, u8 data, size_t len);
+long ipv6_send_packet(struct ipv6_header header, u8 *data, usize len);
 struct ipv6_header ipv6_create_header(u8 src_address[16], u8 dst_address[16],
                                       u8 protocol);
