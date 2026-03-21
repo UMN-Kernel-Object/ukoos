a6caa1757d9b7f9bdb0d08bea6e0d489269ad3c8
KieranMusser <59939188+KieranMusser@users.noreply.github.com>
2026-03-21 16:02:25 -0500

    Add list init for netdevs
    Signed-off-by: KieranMusser <59939188+KieranMusser@users.noreply.github.com>

---
+++ src/kernel/devices/netdev.c
@@ -0,0 +1,9 @@
+/*
+ * SPDX-FileCopyrightText: ukoOS Contributors
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+#include <devices/netdev.h>
+
+struct list_head netdevs = LIST_INIT(netdevs);
