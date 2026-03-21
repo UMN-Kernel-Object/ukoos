e6025e2ca4f83cb2f5c01a09ee82133ab23a6b78
jastintime <130672671+jastintime@users.noreply.github.com>
2026-03-21 11:21:13 -0500

    ipv6.h: fix size of fields, add static_assert
    we don't have 128 bit integers in the kernel instead we can treat the
    addresses as  128 bit wide arrays.
    
    static_assert to make sure the header is the required 40 octets.
    
    comment out unusued structs, we don't have to worry about routing or
    options as we are not routers or interacting with neighbors.
    
    Signed-off-by: jastintime <130672671+jastintime@users.noreply.github.com>

--- src/kernel/include/net/ipv6.h
+++ src/kernel/include/net/ipv6.h
@@ -9,19 +9,22 @@
 
 enum IPv6ProtoNum : u8 { HOPOPT = 0, ICMP = 1, TCP = 6, UDP = 17 };
 
+struct ip_address {
+  u8 addr[16];
+};
+
 struct ipv6_header {
   u8 vtf[4];
   u16 payload_len;
   u8 next_header;
   u8 hop_limit;
-  // TODO: put these in a struct ip_address
-  u8 src[16];
-  u8 dst[16];
+  struct ip_address src;
+  struct ip_address dst;
 };
 
 static_assert(sizeof(struct ipv6_header) == 40);
 
-    struct hop_by_hop_opt_header {
+struct hop_by_hop_opt_header {
   u8 next_header;
   u8 hdr_ext_len;
   u8 *options; // https://datatracker.ietf.org/doc/html/rfc8200#section-4.3
@@ -53,5 +56,6 @@ struct fragment_header {
 //
 
 long ipv6_send_packet(struct ipv6_header header, u8 *data, usize len);
-struct ipv6_header ipv6_create_header(u8 src_address[16], u8 dst_address[16],
+struct ipv6_header ipv6_create_header(struct ip_address,
+                                      struct ip_address dst_address,
                                       u8 protocol);
