3c47ec32ffbcb85d2c70dc1b2cbe1884ea147751
june-fish <git@june.fish>
2026-03-21 23:03:55 -0500

    add duplicate address detection
    Signed-off-by: june-fish <git@june.fish>

--- src/kernel/net/ndp.c
+++ src/kernel/net/ndp.c
@@ -13,12 +13,29 @@ static const struct ip_address LINK_LOCAL = {
 static const struct ip_address ALL_ROUTERS = {
     .addr = {0xff, 0x02, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0,
              0x0, 0x0, 0x2}};
+static const struct ip_address UNSPECIFIED = {
+    .addr = {0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0,
+             0x0, 0x0, 0x0}};
+static const struct ip_address SOLICITED_NODE = {
+    .addr = {0xff, 0x02, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x1, 0xff,
+             0x9a, 0xbc, 0xde}};
 
 int ndp_discover_routers() {
   struct icmpv6_header icmpv6_header = icmpv6_create_header(
       LINK_LOCAL, ALL_ROUTERS, nullptr, 0, NDP_ROUTER_SOLICIT, 0);
   struct ipv6_header ipv6_header =
       ipv6_create_header(LINK_LOCAL, ALL_ROUTERS, ICMP);
+  ipv6_header.hop_limit = 255;
+  ipv6_send_packet(ipv6_header, (u8 *)&icmpv6_header, sizeof(icmpv6_header));
+
+  return 0;
+};
+
+int ndp_duplicate_address_detection() {
+  struct icmpv6_header icmpv6_header = icmpv6_create_header(
+      UNSPECIFIED, SOLICITED_NODE, nullptr, 0, NDP_NEIGHBOR_SOLICIT, 0);
+  struct ipv6_header ipv6_header =
+      ipv6_create_header(UNSPECIFIED, SOLICITED_NODE, ICMP);
   ipv6_header.hop_limit = 255;
   ipv6_send_packet(ipv6_header, (u8 *)&icmpv6_header, sizeof(icmpv6_header));
 
