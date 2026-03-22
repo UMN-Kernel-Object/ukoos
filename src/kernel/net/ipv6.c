eccbbefdc6532ca34899bf9c0821d22222aa6051
jastintime <130672671+jastintime@users.noreply.github.com>
2026-03-21 19:53:09 -0500

    ipv6.c: add create_header() and send_packet().
    Signed-off-by: jastintime <130672671+jastintime@users.noreply.github.com>

--- src/kernel/net/ipv6.c
+++ src/kernel/net/ipv6.c
@@ -1,35 +1,39 @@
-#include <net/ipv6.h>
-#include <net/eth.h>
-#include <devices/netdev.h>
+/* SPDX-FileCopyrightText: ukoOS Contributors
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
 #include <container.h>
+#include <devices/netdev.h>
 #include <endian.h>
+#include <net/eth.h>
+#include <net/ipv6.h>
 
-struct ipv6_header ipv6_create_header(u8 src_address[static 16],
-                                      u8 dst_address[static 16], u8 protocol) {
-	struct ipv6_header header = {0};
-	header.vtf[0] = 0x60;
-	header.next_header = protocol;
-	header.hop_limit = 64;
+struct ipv6_header ipv6_create_header(struct ip_address src_address,
+                                      struct ip_address dst_address,
+                                      u8 protocol) {
+  struct ipv6_header header = {0};
+  header.vtf[0] = 0x60;
+  header.next_header = protocol;
+  header.hop_limit = 64;
 
-
-	memcpy(header.src, src_address, 16 * sizeof(u8));
-	memcpy(header.dst, dst_address, 16 * sizeof(u8));
-	return header;
+  memcpy(&header.src, &src_address, sizeof(header.src));
+  memcpy(&header.dst, &dst_address, sizeof(header.dst));
+  return header;
 }
 
 long ipv6_send_packet(struct ipv6_header header, u8 *data, usize len) {
-  header.payload_len = native_to_big( (u16) len);
+  header.payload_len = native_to_big((u16)len);
   usize bufsiz = len + sizeof(struct ipv6_header);
   u8 *buf = alloc(bufsiz);
-	if (!buf) {
-		return 1;
-	}
+  if (!buf) {
+    return 1;
+  }
 
   memcpy(buf, &header, sizeof(header));
   memcpy(buf + sizeof(header), data, len);
 
   struct netdev *netdev = container_of(netdevs.next, struct netdev, list);
   eth_send_packet(netdev, netdev->ops->get_mac(netdev), buf, bufsiz);
-	free(buf);
-	return 0;
+  free(buf);
+  return 0;
 }
