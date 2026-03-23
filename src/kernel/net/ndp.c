3abdc475c3418effcaaf7b9da1a8b9bbbd2ee9a9
metcya <metcya@gmail.com>
2026-03-22 20:57:44 -0500

    Implement neighbor address discovery

--- src/kernel/net/ndp.c
+++ src/kernel/net/ndp.c
@@ -4,6 +4,7 @@
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
+#include <mm/alloc.h>
 #include <net/icmpv6.h>
 #include <net/ipv6.h>
 
@@ -20,6 +21,8 @@ static const struct ip_address SOLICITED_NODE = {
     .addr = {0xff, 0x02, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x1, 0xff,
              0x9a, 0xbc, 0xde}};
 
+#define CEIL(x, n) ((x) / (n) + ((x) % (n)) ? 1 : 0) * (n)
+
 int ndp_discover_routers() {
   struct icmpv6_header icmpv6_header = icmpv6_create_header(
       LINK_LOCAL, ALL_ROUTERS, nullptr, 0, NDP_ROUTER_SOLICIT, 0);
@@ -30,6 +33,37 @@ int ndp_discover_routers() {
 
   return 0;
 };
+
+struct mac_address
+ndp_resolve_neighbor_address(struct ip_address src_address,
+                             struct ip_address target_address,
+                             struct mac_address src_mac_address) {
+  // NDP options must be 8 byte aligned
+  usize opt_len =
+      CEIL(sizeof(struct option_info) + sizeof(struct mac_address), 8);
+  usize len = sizeof(struct ndp_neighbor_solicit) + opt_len;
+  struct ndp_option option_info = {
+      .type = NDP_SRC_ADDR,
+      .len = opt_len,
+  };
+  u8 *message = alloc(len);
+  memset(message, 0, opt_len);
+  memcpy(message + sizeof(struct ndp_neighbor_solicit), &option_info,
+         sizeof(struct ndp_option));
+  memcpy(message + sizeof(struct ndp_neighbor_solicit) +
+             sizeof(struct ndp_option),
+         &src_mac_address, sizeof(struct mac_address));
+  memcpy(message + sizeof(struct icmpv6_header), &target_address,
+         sizeof(struct ipv6_address)) struct icmpv6_header icmpv6_header =
+      icmpv6_create_header(
+          src_address, target_address, header + sizeof(struct icmpv6_header),
+          len - sizeof(struct icmpv6_header), NDP_NEIGHBOR_SOLICIT, 0);
+  struct ipv6_header ipv6_header =
+      ipv6_create_header(src_address, target_address, ICMP);
+  ipv6_send_packet(ipv6_header, (u8 *)&message, sizeof(message));
+
+  return 0;
+}
 
 int ndp_duplicate_address_detection() {
   struct icmpv6_header icmpv6_header = icmpv6_create_header(
