17419f86a724f5407d62207b10a0fd575149a611
june-fish <git@june.fish>
2026-04-08 20:42:14 -0500

    add icmpv6_solicit_routers
    Signed-off-by: june-fish <git@june.fish>

--- src/kernel/net/icmpv6.c
+++ src/kernel/net/icmpv6.c
@@ -22,3 +22,33 @@ struct icmpv6_header icmpv6_create_header(struct ip_address src,
 
   return header;
 }
+
+int icmpv6_solicit_routers() {
+  // address presumably not yet assigned
+  struct ip_address src = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+
+  // all routers multicast
+  struct ip_address dst = {0xff, 0x02, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0x2};
+
+  struct ndp_option opt = {
+      .type = 1, // Source Link-Layer Address
+      .length = 8,
+  };
+
+  struct icmpv6_header icmpv6_header = icmpv6_create_header(
+      src, dst, (u8 *)&opt, sizeof(struct ndp_option), NDP_ROUTER_SOLICIT, 0);
+
+  struct ipv6_header ipv6_header =
+      ipv6_create_header(src, dst, NDP_ROUTER_SOLICIT);
+
+  constexpr usize size =
+      sizeof(struct icmpv6_header) + sizeof(struct ndp_option);
+  u8 buf[size];
+
+  memcpy(buf, &icmpv6_header, sizeof(struct icmpv6_header));
+  memcpy(buf, &opt, sizeof(struct ndp_option));
+
+  ipv6_send_packet(ipv6_header, buf, size);
+
+  return 0;
+}
